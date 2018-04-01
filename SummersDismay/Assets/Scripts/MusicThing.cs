2b97f143079a27e58dd49912476b2488ddddbd9e
Vincent Miller <vmiller0725@gmail.com>
2018-04-01 13:16:54 -0400

    asdf

---
+++ SummersDismay/Assets/Scripts/MusicThing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicThing : MonoBehaviour {
+    public AudioClip[] clips;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().clip = clips[Random.Range(0, clips.Length)];
+        GetComponent<AudioSource>().Play();
+	}
+}
