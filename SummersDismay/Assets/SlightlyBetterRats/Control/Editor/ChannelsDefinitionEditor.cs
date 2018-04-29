aea3818510d25d318f13b6b92abb2c32f17427dd
Vincent Miller <vmiller0725@gmail.com>
2018-04-29 17:03:14 -0400

    asdf

---
+++ SummersDismay/Assets/SlightlyBetterRats/Control/Editor/ChannelsDefinitionEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System;
+
+namespace SBR.Editor {
+    [CustomEditor(typeof(ChannelsDefinition))]
+    public class ChannelsDefinitionEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            if (GUILayout.Button("Generate Class")) {
+                var path = AssetDatabase.GetAssetPath(target);
+                if (path.Length > 0) {
+                    ChannelsClassGenerator.GenerateClass(target as ChannelsDefinition);
+                }
+            }
+
+            DrawDefaultInspector();
+        }
+    }
+}
---
+++ SummersDismay/Assets/SlightlyBetterRats/Time/Magazine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SBR.Editor {
+    public class Magazine {
+        public int capacity { get; set; }
+        public int remaining { get; set; }
+        public float reloadTime {
+            get {
+                return reloadTimer.expiration;
+            }
+
+            set {
+                reloadTimer.expiration = value;
+            }
+        }
+
+        private bool reloading;
+        private ExpirationTimer reloadTimer;
+
+        public Magazine(int capacity, float reload) {
+            remaining = this.capacity = capacity;
+            reloadTimer = new ExpirationTimer(reload);
+        }
+
+        public bool Use() {
+            if (reloading && reloadTimer.expired) {
+                remaining = capacity;
+            }
+
+            if (remaining > 0) {
+                remaining--;
+
+                if (remaining == 0) {
+                    reloading = true;
+                    reloadTimer.Set();
+                }
+
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
